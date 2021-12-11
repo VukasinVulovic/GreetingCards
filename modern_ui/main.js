fef1ca84728babe43add1e8a08cdeda5e8ddcdc1
VukasinVulovic <vukasinvulovic.radninalog@gmail.com>
2021-12-11 12:26:13 +0100

    made responsive

--- modern_ui/main.js
+++ modern_ui/main.js
@@ -1,43 +1,15 @@
 const card = document.querySelector('.card');
-const front = document.querySelector('.front');
-const back = document.querySelector('.back');
-
-async function animate(from, to, step, interval, cb) {
-    return new Promise(resolve => {
-        const loop = () => {
-            if(from >= to) {
-                clearInterval(loop);
-                resolve();
-                return;
-            }
-            
-            cb(from);
-            from += step;
-        }
-
-        setInterval(loop, interval);
-    });
-}
-
-let busy = false;
+const front = card.querySelector('.front');
+const back = card.querySelector('.back');
 let prev = false;
 
-card.addEventListener('click', async() => {
-    if(busy)
-        return;
-
-    busy = true;
-
-    await animate(0, 180, 10, 10, rot => {
-        if(!prev) {
-            front.style.transform = `rotateY(${rot}deg)`;
-            back.style.transform = `rotateY(${180-rot}deg)`;
-        } else {
-            front.style.transform = `rotateY(${180 - rot}deg)`;
-            back.style.transform = `rotateY(${rot}deg)`;
-        }
+window.onload = () => {
+    back.hidden = false;
+    
+    card.addEventListener('click', async() => {
+        front.style.transform = `rotateY(${!prev ? 180 : 0}deg)`;
+        back.style.transform = `rotateY(${!prev ? 0 : 180}deg)`;
+    
+        prev = !prev;
     });
-
-    busy = false;
-    prev = !prev;
-});
+}
