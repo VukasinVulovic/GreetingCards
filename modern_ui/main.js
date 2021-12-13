72e6d9292a99c0d93016f2f9eaef74f1a1c361bc
Vukasin Vulovic <94569468+VukasinVulovic@users.noreply.github.com>
2021-12-13 23:28:31 +0100

    Update main.js

--- modern_ui/main.js
+++ modern_ui/main.js
@@ -6,31 +6,6 @@ const title = front.querySelector('.title');
 const text = back.querySelector('.text');
 
 let prev = false;
-
-const lang = [
-    {
-        'title': 'Happy<br>new<br>year!',
-        'message': 'Students from ITHS would like to wish you a Happy New Year. <br>We\'re happy to have you join us in this wonderful project.'
-    },
-    {
-        'title': 'Срећна<br>нова<br>година!',
-        'message': 'Ученици ИТХС-а вам желе срећну Нову годину.<br>Срећни смо што нам се придружите у овом дивном пројекту.'
-    },
-    {
-        'title': 'Feliz<br>año<br>nuevo!',
-        'message': 'A los estudiantes de ITHS les gustaría desearle un Feliz Año Nuevo. Estamos felices de que se una a nosotros en este maravilloso proyecto.'
-    },
-    {
-        'title': 'Szczęśliwego<br>Nowego<br>Roku.',
-        'message': 'Studenci ITHS życzą Państwu szczęśliwego Nowego Roku.<br>Cieszymy się, że dołączyłeś do nas w tym wspaniałym projekcie.'
-    },
-    {
-        'title': 'Yeni Yılın<br>Kutlu<br>Olsun!',
-        'message': 'Studenci ITHS życzą Państwu szczęśliwego Nowego Roku.<br>Cieszymy się, że dołączyłeś do nas w tym wspaniałym projekcie.'
-    }
-];
-
-
 const map = (val, imin, imax, omin, omax) => (val - imin) * (omax - omin) / (imax - imin) + omin;
 
 async function animate(from, to, step, delay, cb) {
@@ -61,28 +36,4 @@ window.onload = () => {
     
         prev = !prev;
     });
-
-    const change = async() => {
-        if(i >= lang.length)
-            i = 0;
-
-        await animate(0, 1, 0.1, 10, v => {
-            text.style.color = `rgba(0, 0, 0, ${1-v})`;
-            title.style.color = `rgba(0, 0, 0, ${1-v})`;
-        });
-
-        text.innerHTML = lang[i]['message'];
-        title.innerHTML = lang[i++]['title'];
-
-        await delay(10);
-
-        await animate(0, 1, 0.1, 10, v => {
-            title.style.color = `rgba(0, 0, 0, ${v})`;
-            text.style.color = `rgba(0, 0, 0, ${v})`;
-        });
-
-        setTimeout(change, 2500);
-    }
-
-    // setTimeout(change, 2500);
 }
